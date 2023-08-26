9af5dc7c0a62eb6fc8ba7b3685d804dfd08663e8
tokuzou0829 <77785208+tokuzou0829@users.noreply.github.com>
2023-08-26 16:10:10 +0900

    PWA化

---
+++ scripts/service_worker.js
@@ -0,0 +1,22 @@
+var CACHE_NAME = 'pwa-caches';
+var urlsToCache = [
+    '/tokuzou0829.github.io/',
+];
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
